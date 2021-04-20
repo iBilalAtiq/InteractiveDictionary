2dc0b82830da0e8deac16274027afcadcb1c9b0c
Bilal <bilalatiqsd@gmail.com>
2021-04-20 23:42:47 +0500

    Initial commit

---
+++ dictionary.py
@@ -0,0 +1,21 @@
+import json
+
+# Loading json data file
+data = json.load(open('data.json'))
+#print(data)
+
+# getting meaning for a particular word.
+#print(data['rain'])
+
+
+def translate(word_mean):
+
+    if word in data:
+        return data[word_mean]
+    else:
+        return "Word Not Found"
+
+
+word = input("Enter Word: ")
+print(translate(word))
+
