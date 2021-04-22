e5488aea75b87de6aea2b414d26a71897ad097ce
Bilal <bilalatiqsd@gmail.com>
2021-04-22 17:02:48 +0500

    Final Commit

--- dictionary.py
+++ dictionary.py
@@ -1,25 +1,40 @@
 import json
-from difflib import SequenceMatcher, get_close_matches
+from difflib import get_close_matches
 
 # Loading json data file
 data = json.load(open('data.json'))
-#print(data)
+
+
+# print(data)
 
 # getting meaning for a particular word.
-#print(data['rain'])
+# print(data['rain'])
 
 
 def translate(word_mean):
-
     word_lower = word_mean.lower()
     similarity = get_close_matches(word_lower, data.keys())[0]
     if word_lower in data:
-        if word_lower == similarity:
-            print(f"{similarity}")
         return data[word_lower]
+    elif len(similarity) > 0:
+        print(f"Did you mean {similarity}? If yes type Yes else No")
+        user_input = input("Yes or No: ")
+        if user_input.lower() == 'yes':
+            print(data[similarity])
+        elif user_input.lower() == 'no':
+            return "Word does not exist"
+        else:
+            return "Word not found"
     else:
-        return "Word Not Found"
+        return "Word does not exist"
 
 
 word = input("Enter Word: ")
-print(translate(word))
+
+output = translate(word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+    else:
+        print(output)
